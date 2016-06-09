c641f91199ae3f74c77c4de79e9d72e859df69bf
Tyler Jackson <five1thousand@gmail.com>
2016-06-08 20:20:55 -0400

    v0.1.0 String Fingerprinting
    - Adding HString hashing functionality. UNTESTED

--- inc/HString.h
+++ inc/HString.h
@@ -7,33 +7,35 @@ class HString final
 
 	//ATTRIBUTES
 private:
-	const uint64 hash;
+	/// A (probably) unique value that can be used to identify the string. Chance of collisions should be very low.
+	const uint64 fingerprint;
+
+	/// The string that this HString holds a fingerprint for.
 	const std::string string;
 
 	// CONSTRUCTORS / DESTRUCTORS
 protected:
-	HString(const char* cstr, uint32 argc, uint64 sHash);
-	~HString() {};
+	HString(const char* cstr);
+	~HString() {} // nothing special needs to be done here
 
 	// METHODS
 public:
-	static bool operator== (const HString& lhs, const HString& rhs);
-	static bool operator== (const HString& lhs, const std::string& rhs);
-	static bool operator== (const std::string& lhs, const HString& rhs);
-	static bool operator== (const HString& lhs, const char* rhs);
-	static bool operator== (const char* lhs, const HString& rhs);
-
-	char operator[] (uint32 pos);
+	bool operator== (const HString& rhs) const;
+	bool operator== (const std::string& rhs) const;
+	bool operator== (const char* rhs) const;
 
 	int32 compare(const HString& other) const;
 	int32 compare(const std::string& other) const;
 	int32 compare(const char* other) const;
 
-	uint32 length();
+private:
+	static uint64 hashFunction(const char* cstr);
 
 	// GETTERS / SETTERS
 public:
-	const uint64 getHash();
-	const std::string* getString();
-	const char* getCString();
+	char operator[] (uint32 pos) const;
+	uint32 length() const;
+	uint64 getFingerprint() const;
+	const std::string* getString() const;
+	const char* getCString() const;
 };
---
+++ src/HString.cpp
@@ -0,0 +1,107 @@
+#include "stdafx.h"
+#include "HString.h"
+
+
+// CONSTRUCTORS / DESTRUCTORS
+
+HString::HString(const char* cstr) : 
+	string(cstr),
+	fingerprint(hashFunction(cstr))
+{}
+
+// METHODS
+
+bool HString::operator== (const HString& rhs) const
+{
+	return fingerprint == rhs.getFingerprint();
+}
+
+
+bool HString::operator== (const std::string& rhs) const
+{
+	return string.compare(rhs) == 0;
+}
+
+
+bool HString::operator== (const char* rhs) const
+{
+	return string.compare(rhs) == 0;
+}
+
+
+int32 HString::compare(const HString& other) const
+{
+	uint64 tmp = other.getFingerprint();
+	return fingerprint == tmp ? 0 : (fingerprint > tmp ? 1 : -1);
+}
+
+
+int32 HString::compare(const std::string& other) const
+{
+	return string.compare(other);
+}
+
+
+int32 HString::compare(const char* other) const
+{
+	return string.compare(other);
+}
+
+
+uint64 HString::hashFunction(const char* cstr)
+{
+	// processes string 4 characters at a time in order to build a (probably) unique fingerprint value
+	uint64 hash = 0;
+	uint32 idx = 0;
+	uint32 len = -1;
+
+	while (cstr[++len] != 0); // increment len until cstr[len] is the null terminator
+	
+	const uint32 wLength = len / 4;
+	const uint64 MOD_VAL = 4294967291; // largest 32-bit prime
+	const uint32* intString = (const uint32*)cstr;
+
+	while (idx < wLength)
+	{
+		hash <<= 32; // shift previous result to clear room for next 4 chars
+		hash |= intString[idx++]; // move the next for chars from cstr into the now-cleared section of hash and increment idx
+		hash %= MOD_VAL; // mod hash with MOD_VAL to create fingerprint
+	}
+
+	hash <<= 32;
+	hash |= intString[idx] >> ((4 - len % 4) * 8); // remove excess bits from after the end of the string
+	hash %= MOD_VAL;
+
+	return hash;
+}
+
+// GETTERS / SETTERS
+
+char HString::operator[](uint32 idx) const
+{
+	return string[idx];
+}
+
+
+uint32 HString::length() const
+{
+	return string.length();
+}
+
+
+uint64 HString::getFingerprint() const
+{
+	return fingerprint;
+}
+
+
+const std::string* HString::getString() const
+{
+	return &string;
+}
+
+
+const char* HString::getCString() const
+{
+	return string.c_str();
+}
